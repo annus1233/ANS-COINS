4bd5f99324e56d4282fae4d892c501e53393e25b
annus1233 <javaid1ahmad1rkw1@gmail.com>
2025-01-29 00:27:59 -0800

    Update db.php

--- New folder (2)/db.php
+++ New folder (2)/db.php
@@ -1,14 +1,1 @@
-// db.php
-<?php
-$host = 'localhost';
-$dbname = 'goldfish';  
-$username = 'gold fish';  
-$password = 'mrvanish';  
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, );
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
